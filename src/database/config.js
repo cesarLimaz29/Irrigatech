51427f6fe4c408fb2da4bbcddec57b57456d919a
matheus-rosa11 <matheus.rosa@sptech.school>
2022-05-04 10:34:01 -0300

    arrumado erro na tela de login e funções

--- src/database/config.js
+++ src/database/config.js
@@ -20,9 +20,9 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
     host: "localhost",
-    user: "root",
+    user: "aluno",
     database: "IRRIGATECH",
-    password: "80447341Mm^^",
+    password: "sptech",
 };
 
 function executar(instrucao) {
