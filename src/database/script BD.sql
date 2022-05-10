104843a3762b459c5f45f154b4f068696787ec16
tamirez <tamirez.melo@sptech.school>
2022-05-10 14:48:40 -0300

    Correção da modelagem e script

--- src/database/script BD.sql
+++ src/database/script BD.sql
@@ -4,41 +4,43 @@ USE IRRIGATECH;
 
 CREATE TABLE Usuario(
 id_usuario INT PRIMARY KEY AUTO_INCREMENT,
-cpf CHAR(11),
-nome_cli VARCHAR(100),
-email VARCHAR(45),
-senha VARCHAR(45),
-tel_cel VARCHAR(15)
+cpf CHAR(11) NOT NULL,
+nome_cli VARCHAR(100) NOT NULL,
+email VARCHAR(45) NOT NULL,
+senha VARCHAR(45) NOT NULL,
+tel_cel VARCHAR(15),
+fk_empresa INT NOT NULL,
+fk_administrador INT,
+foreign key (fk_administrador) references usuario(id_usuario)
 );
 
 CREATE TABLE Empresa(
 id_empresa INT PRIMARY KEY AUTO_INCREMENT,
-cnpj CHAR(14),
-nome_emp VARCHAR(100),
-segmento VARCHAR(60),
+cnpj CHAR(14) NOT NULL,
+nome_emp VARCHAR(100) NOT NULL,
 tel_com VARCHAR(15),
 fkempresa_filiais INT,
 FOREIGN KEY (fkempresa_filiais) REFERENCES empresa(Id_empresa)
 );
 
-CREATE TABLE login (
-fk_usuario INT,
-fk_empresa INT,
-email_funcionario VARCHAR(45),
-senha_funcionario VARCHAR(45),
-nome_funcionario VARCHAR(45),
-foreign key (fk_usuario) references usuario(id_usuario),
-foreign key (fk_empresa) references empresa(id_empresa),
-primary key (fk_usuario, fk_empresa)
-);
+alter table usuario add foreign key (fk_empresa) references empresa(id_empresa);
 
 CREATE TABLE Setor(
 id_setor INT PRIMARY KEY AUTO_INCREMENT,
-setor VARCHAR(45),
+setor VARCHAR(45) NOT NULL,
 fkempresa INT,
 FOREIGN KEY (fkempresa) REFERENCES empresa(Id_empresa)
-
 );
+
+CREATE TABLE acesso(
+id_acesso INT auto_increment,
+fk_usuario INT,
+foreign key (fk_usuario) references usuario(id_usuario),
+fk_setor INT,
+foreign key (fk_setor) references setor(id_setor),
+primary key (id_acesso, fk_usuario, fk_setor)
+);
+
 
 CREATE TABLE Sensor(
 id_sensor INT PRIMARY KEY AUTO_INCREMENT,
