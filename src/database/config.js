ce57dbef06f4cf60ec45a50ff1d979ef36852c2f
tamirez <tamirez.melo@sptech.school>
2022-05-10 11:49:31 -0300

    Modelagem BD atualizada

--- src/database/config.js
+++ src/database/config.js
@@ -20,9 +20,9 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
     host: "localhost",
-    user: "aluno",
+    user: "root",
     database: "IRRIGATECH",
-    password: "sptech",
+    password: "27834350",
 };
 
 function executar(instrucao) {
