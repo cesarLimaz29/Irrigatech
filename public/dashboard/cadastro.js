9f5552b0d3c40721255b31dd071f8e14083b35e8
guianastacio <guilherme.anastacio@sptech.school>
2022-05-24 00:22:39 -0300

    atualizando cadastro

---
+++ public/dashboard/cadastro.js
@@ -0,0 +1,15 @@
+// function acao() {
+//     let modal = document.querySelector(".modal")
+//     modal.style.display = 'block';
+// }
+// function fechar() {
+//     let modal = document.querySelector(".modal")
+//     modal.style.display = 'none';
+// }
+
+function visualizar(pagina) {
+    document.body.setAttribute('page',pagina)
+    if (pagina == 'cadastro') {
+        document.getElementById('nome').focus();
+    }
+}
