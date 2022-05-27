8e61fb47168ec20209600523562f3545fc5ff399
Landineo <lucas.rodrigues@sptech.school>
2022-05-27 08:21:01 -0300

    push na branch test

--- site/src/models/medidaModel.js
+++ site/src/models/medidaModel.js
@@ -1,25 +1,25 @@
 var database = require("../database/config");
 
 function buscarUltimasMedidas(idAquario, limite_linhas) {
-    instrucaoSql = `select 
-                        temperatura, 
-                        umidade, 
-                        hora_sensor,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    from hora_sensor
-                    where fksensor = ${idAquario}
-                    order by id desc limit ${limite_linhas}`;
+    instrucaoSql = `
+    select umidade,
+        temperatura,
+            hora_sensor,
+                    from Dados_sensor 
+                        where fksensor = ${idAquario}
+                            order by id desc limit ${limite_linhas}`;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
 function buscarMedidasEmTempoReal(idAquario) {
-    instrucaoSql = `select 
-                        temperatura, 
-                        umidade, DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fksensor 
-                        from medida where fk_aquario = ${idAquario} 
-                    order by id desc limit 1`;
+    instrucaoSql = `
+    select umidade,
+        temperatura,
+            hora_sensor,
+                    from Dados_sensor 
+                        where fksensor = ${idAquario} 
+                            order by id desc limit 1`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
