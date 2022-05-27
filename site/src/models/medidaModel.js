67cca2956a9aff24cadd92317bd7c6fb45b37c28
landineo <lucas.rodrigues@sptech.school>
2022-05-27 14:02:06 -0300

    tentativa #1 de funcionamento da dashboard no banco

--- site/src/models/medidaModel.js
+++ site/src/models/medidaModel.js
@@ -1,12 +1,7 @@
 var database = require("../database/config");
 
 function buscarUltimasMedidas(idAquario, limite_linhas) {
-    instrucaoSql = `select 
-                        temperatura, 
-                        umidade, 
-                        hora_sensor,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    from hora_sensor
+    instrucaoSql = `select umidade, temperatura, hora_sensor
                     where fksensor = ${idAquario}
                     order by id desc limit ${limite_linhas}`;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
@@ -14,11 +9,8 @@ function buscarUltimasMedidas(idAquario, limite_linhas) {
 }
 
 function buscarMedidasEmTempoReal(idAquario) {
-    instrucaoSql = `select 
-                        temperatura, 
-                        umidade, DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fksensor 
-                        from medida where fk_aquario = ${idAquario} 
+    instrucaoSql = `select umidade, temperatura, hora_sensor
+                    where fk_aquario = ${idAquario} 
                     order by id desc limit 1`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
